a00431d52858f7f0826319bdea8b1da659e64a3b
Anirutha <aniruthasivakumar03@gmail.com>
2023-09-14 07:34:49 +0530

    all changes done

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/send-email", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`server is running on port:${PORT}`);
